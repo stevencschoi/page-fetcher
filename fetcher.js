fe5eac23ae408228e5cdc1d264934dbb389cf6cc
Steven Choi <stevencschoi87@gmail.com>
2020-02-13 01:48:23 +0000

    refactored bytes to use asynchronously

--- fetcher.js
+++ fetcher.js
@@ -1,19 +1,31 @@
 const request = require('request');
 const fs = require('fs');
+// const readline = require('readline');
+
 const url = process.argv[2];
 const filePath = process.argv[3];
 
-const getFilesizeInBytes = (filename) => {
-  const stats = fs.statSync(filename);
-  const fileSizeInBytes = stats["size"];
-  return fileSizeInBytes;
-};
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
 
 const fetcher = (url, filePath) => {
+  // if (filePath) {
+  //   // rl.pause();
+  //   rl.question('This file exists. Press Y to overwrite the file, or Ctrl+C to exit the app', (key) => {
+  //     if (key === Y) {
+  //       close();
+  //     }
+  //   });
+  // }
   request(url, (error, response, body) => {
     fs.writeFile(filePath, body, (err) => {
-      if (err) throw err;
-      console.log(`Downloaded and saved ${getFilesizeInBytes(filePath)} bytes to ${filePath}`);
+      fs.stat(filePath, (err, stats) => {
+        const size = stats.size;
+        if (err) throw err;
+        console.log(`Downloaded and saved ${size} bytes to ${filePath}`);
+      })
     });
   });
 };
